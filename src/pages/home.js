a7993f844b7886eefec390272bb4117b3fe673b4
Dominic Canare <dom@makeict.org>
2019-03-31 15:55:54 -0500

    Add home intro + visual tweaks

--- src/pages/home.js
+++ src/pages/home.js
@@ -48,8 +48,9 @@ export default class Page extends Component {
 				<div className="content">
 					<img src={headshot} width="256" height="256" className="logo" alt="Dominic Canare headshot"/>
 					<h3>About me</h3>
-					<p>Hi, there! My name is Dominic Canare, and I'm a PhD candidate in <a href="https://en.wikipedia.org/wiki/Human_factors_and_ergonomics#Cognitive_ergonomics" target="_blank" rel="noopener noreferrer">Human Factors Psychology</a> at <a href="https://www.wichita.edu/academics/fairmount_college_of_liberal_arts_and_sciences/psychology/graduate/humanfactors/humanfactorsphd.php" target="_blank" rel="noopener noreferrer">Wichita State University</a>. My research interests are primarily focused on <a href="https://en.wikipedia.org/wiki/Human%E2%80%93computer_interaction" target="_blank" rel="noopener noreferrer">human-computer interaction</a>, experience design, sensory substitution/augmentation, and adaptive technologies.</p>
-					<p>Presently, I am seeking internship opportunities in Human Factors and HCI.</p>
+					<p>Hi, there! My name is Dominic Canare, and I'm a PhD candidate in <a href="https://en.wikipedia.org/wiki/Human_factors_and_ergonomics#Cognitive_ergonomics" target="_blank" rel="noopener noreferrer">Human Factors Psychology</a> at <a href="https://www.wichita.edu/academics/fairmount_college_of_liberal_arts_and_sciences/psychology/graduate/humanfactors/humanfactorsphd.php" target="_blank" rel="noopener noreferrer">Wichita State University</a>. I'm most interested in research on <a href="https://en.wikipedia.org/wiki/Human%E2%80%93computer_interaction" target="_blank" rel="noopener noreferrer">human-computer interaction</a>, sensory substitution/augmentation, and assistive/adaptive technologies. I believe that a universal design approach to problems will yield the best possible outcomes.</p>
+					<p>I'm a scientist, a problem-solver, a software developer, a community leader, and an activist, and I'm always looking for ways to use my talent and skill to improve lives and reduce harm.</p>
+					<p>Additionally, I am presently seeking contract and internship opportunities in Human Factors and HCI.</p>
 
 					<h3>Connect with me!</h3>
 					<div className="contact-blocks">
