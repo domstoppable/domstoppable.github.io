305f8a542f423daa1229fddf0bcf3dad04ffb1dd
Dominic Canare <dom@greenlightgo.org>
2019-12-15 00:57:20 -0800

    Updates

---
+++ precache-manifest.64351c180247fa297d5629b54b3664d8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d011f1dcf60dad24a4af1c3e52cafbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec7c3ac3901e633b575f",
+    "url": "/static/css/main.cda1b857.chunk.css"
+  },
+  {
+    "revision": "5313d57928f65e24a2cc",
+    "url": "/static/js/2.fe8afa7c.chunk.js"
+  },
+  {
+    "revision": "38e8b4df4d0f37808ed8a413554ad308",
+    "url": "/static/js/2.fe8afa7c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ec7c3ac3901e633b575f",
+    "url": "/static/js/main.7b29f037.chunk.js"
+  },
+  {
+    "revision": "32c77e96511c1d0ba69b",
+    "url": "/static/js/runtime-main.2598488a.js"
+  },
+  {
+    "revision": "83e5fcc3f7879a34e77a5e27cae690ed",
+    "url": "/static/media/academics-icon.83e5fcc3.svg"
+  },
+  {
+    "revision": "6a24b81658e08bbd9326f936b867b35c",
+    "url": "/static/media/awards-icon.6a24b816.svg"
+  },
+  {
+    "revision": "eca08cdf408cca675d18656fdaf59a75",
+    "url": "/static/media/community-icon.eca08cdf.svg"
+  },
+  {
+    "revision": "b22a4a0cafefb38158a501120b61590b",
+    "url": "/static/media/dominic-canare.b22a4a0c.png"
+  },
+  {
+    "revision": "103f6f3f6d5a7cdad7ba921be46b4b34",
+    "url": "/static/media/experience-icon.103f6f3f.svg"
+  },
+  {
+    "revision": "a315b82412d4a91bc5301dd7f9590c63",
+    "url": "/static/media/progress-mask.a315b824.svg"
+  },
+  {
+    "revision": "dc78b3bf8cc61882d9fd72ac4a8cf036",
+    "url": "/static/media/publications-icon.dc78b3bf.svg"
+  },
+  {
+    "revision": "7c215e82432e7431ee2390b1ed0ac07d",
+    "url": "/static/media/wave-icon.7c215e82.svg"
+  }
+]);
