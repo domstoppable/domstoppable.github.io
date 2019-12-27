fdd1d96e921b4b9f559957bcdd8360ec52471eb1
Dominic Canare <dom@greenlightgo.org>
2019-12-27 13:04:04 -0600

    Updates

--- precache-manifest.6d1eaee88b6929ab614e82aad8aed96a.js
+++ precache-manifest.6d1eaee88b6929ab614e82aad8aed96a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8c5c7952fc52df43cb8bfffd42108c27",
+    "revision": "e7515aa149e295b388fb8d00dd8fb730",
     "url": "/index.html"
   },
   {
-    "revision": "ef729ba33daf333c98d3",
+    "revision": "a96b7ae57f56e109cdfe",
     "url": "/static/css/main.cda1b857.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.fe8afa7c.chunk.js.LICENSE"
   },
   {
-    "revision": "ef729ba33daf333c98d3",
-    "url": "/static/js/main.a2fc58b5.chunk.js"
+    "revision": "a96b7ae57f56e109cdfe",
+    "url": "/static/js/main.66c75a74.chunk.js"
   },
   {
     "revision": "32c77e96511c1d0ba69b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2b5baa8566be65dedc9c282c611fc240.js"
+  "/precache-manifest.6d1eaee88b6929ab614e82aad8aed96a.js"
 );
 
 self.addEventListener('message', (event) => {
