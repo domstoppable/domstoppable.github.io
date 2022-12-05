b17e5bfbf690983fe50c5f09bb1fca2a692c9db7
Dominic Canare <dom@greenlightgo.org>
2022-12-05 16:43:22 -0600

    Text updates

--- src/pages/home.js
+++ src/pages/home.js
@@ -24,10 +24,6 @@ export default class Page extends Component {
 				uri: 'https://www.researchgate.net/profile/Dominic_Canare',
 				icon: 'research-gate-icon.png'
 			},{
-				name: 'Twitter/obi_dom',
-				uri: 'https://www.twitter.com/obi_dom',
-				icon: 'twitter-icon.svg'
-			},{
 				name: 'YouTube/domstoppable',
 				uri: 'https://www.youtube.com/user/domstoppable',
 				icon: 'youtube-icon.svg'
@@ -41,8 +37,8 @@ export default class Page extends Component {
 				<div className="content">
 					<img src={headshot} width="256" height="256" className="logo" alt="Dominic Canare headshot"/>
 					<h3>About me</h3>
-					<p>Hi, there! My name is Dominic Canare, and I'm a PhD candidate in <a href="https://en.wikipedia.org/wiki/Human_factors_and_ergonomics#Cognitive_ergonomics" target="_blank" rel="noopener noreferrer">Human Factors Psychology</a> at <a href="https://www.wichita.edu/academics/fairmount_college_of_liberal_arts_and_sciences/psychology/graduate/humanfactors/humanfactorsphd.php" target="_blank" rel="noopener noreferrer">Wichita State University</a>. I'm most interested in research on <a href="https://en.wikipedia.org/wiki/Human%E2%80%93computer_interaction" target="_blank" rel="noopener noreferrer">human-computer interaction</a>, sensory substitution/augmentation, and assistive/adaptive technologies. I believe that a universal design approach to problems will yield the best possible outcomes.</p>
-					<p>I'm a scientist, a problem-solver, a software developer, an educator, a community leader, and an activist, and I'm always looking for ways to use my talent and skill to improve lives and reduce harm. I have a passion for making technologies more accessible, efficient, and  effective through the thoughtful use of artificial intelligence, automation, and data-driven interaction design.</p>
+					<p>Hi, there! My name is Dominic Canare, and I'm interested in research on <a href="https://en.wikipedia.org/wiki/Human%E2%80%93computer_interaction" target="_blank" rel="noopener noreferrer">human-computer interaction</a>, sensory substitution/augmentation, and assistive/adaptive technologies. I believe that we should strive to make every human experience as inclusive as possible, and I try to apply my skillset to help make that possible.</p>
+					<p>I'm a scientist, a problem-solver, a software developer, an educator, a community leader, and an activist. I'm always looking for ways to use my skill to improve lives and reduce harm. I have a passion for making technologies more accessible, efficient, and effective through the thoughtful use of artificial intelligence, automation, and data-driven interaction design.</p>
 
 					<h3>Connect with me!</h3>
 					<ul className="contact-blocks">
